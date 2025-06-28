b37b28c722a903afc9beaefaa67405869cfbe1f2
technovasystem <soporte.technovasystem@gmail.com>
2025-06-28 02:57:26 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import login
+from routers import login
 
 app = FastAPI()
 
+# CORS para que tu app móvil o web pueda acceder al backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # O ajusta a los dominios seguros
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Incluir las rutas del login
 app.include_router(login.router)
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "BoliBank SYSTEM funcionando 😎"}
