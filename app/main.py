440179a89e6f84fb8bb3a28d207abf2129791a55
technovasystem <soporte.technovasystem@gmail.com>
2025-06-28 02:55:08 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors 
-import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import login
 
 app = FastAPI()
